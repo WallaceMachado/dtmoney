1d111f53141ee897fecf254a1da2c274e3183476
wallace machado <wallacemachado@ntopus.com>
2022-07-30 17:08:17 -0300

    react-modal implementação inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
+import Modal from "react-modal";// radd react-modal / @types/react-modal
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+// acessibilidade -> referenciar o elemento root alertando impossibilidade de acesso aos elementos abaixo do modal
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    // toda função executada a partir do usuário começar com handle
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>  
-       <Header />
+       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
        <Dashboard />
+       <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+      >
+          <h2>Cadastrar transação</h2>
+      </Modal>
        <GlobalStyle />
     </>
   );
