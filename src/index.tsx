85ee3eb5eeb2d7e0a645c983a7fdfee368d5afa7
wallace machado <wallacemachado@ntopus.com>
2022-07-30 15:55:21 -0300

    add back-end-api fake com miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer, Model } from "miragejs";
 import {App} from './App';
+
+
+createServer({
+  // routes -> rotas da api fictícia
+  routes() {
+    // endereçamento inserido no useEffect
+    this.namespace = "api";
+
+    // quando houver request get na rota "./api/transactions" -> retornar objeto
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
