4e40346fa071bddaadda0b57eae6bf068c471fad
wallace machado <wallacemachado@ntopus.com>
2022-07-30 18:04:27 -0300

    add componente new transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Modal from "react-modal";// radd react-modal / @types/react-modal
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
 import { GlobalStyle } from "./styles/global";
 
 // acessibilidade -> referenciar o elemento root alertando impossibilidade de acesso aos elementos abaixo do modal
@@ -22,12 +24,10 @@ export function App() {
     <>  
        <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
        <Dashboard />
-       <Modal
+       < NewTransactionModal
           isOpen={isNewTransactionModalOpen}
           onRequestClose={handleCloseNewTransactionModal}
-      >
-          <h2>Cadastrar transação</h2>
-      </Modal>
+      />        
        <GlobalStyle />
     </>
   );
