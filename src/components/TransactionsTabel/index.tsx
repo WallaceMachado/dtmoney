bbeb7d8afdcf19137e96a4cfec8d0aba002f6e8f
wallace machado <wallacemachado@ntopus.com>
2022-07-31 09:17:07 -0300

    formantando valores com biblioteca nativa intl

--- src/components/TransactionsTabel/index.tsx
+++ src/components/TransactionsTabel/index.tsx
@@ -34,10 +34,18 @@ export function TransactionsTable() {
             return (
               <tr key={transaction.id}>
                 <td>{transaction.title}</td>
-                <td className={transaction.type}>{transaction.amount}</td>             
+                <td className={transaction.type}>
+                  {new Intl.NumberFormat("pt-BR", {// biblioteca nativa Intl
+                    style: "currency",// tipo em moeda
+                    currency: "BRL",
+                  }).format(transaction.amount)}
+                </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt} </td>               
-              
+                <td>
+                  {new Intl.DateTimeFormat("pt-BR").format(
+                    new Date(transaction.createdAt)
+                  )}
+                </td>
               </tr>
             );
           })}
